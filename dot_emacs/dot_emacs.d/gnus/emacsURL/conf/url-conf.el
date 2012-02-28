65de360a206e56eba63dc9fb4eb720f59020b261
Guillaume Sadegh <sadegh@lrde.epita.fr>
2012-02-28 15:46:52 +0100

    Remove old emacs

--- dot_emacs/dot_emacs.d/gnus/emacsURL/conf/url-conf.el
+++
@@ -1,9 +0,0 @@
-
-;; la vielle feinte autour du load-path.
-;;
-(setq load-path                                                              
-      (push                                                                  
-       "/u/guest/dp/public/emacsURL/emacs/site-lisp/"
-       load-path))
-
-(require 'url)
--- dot_emacs/dot_emacs.d/lpath.el
+++
@@ -1,4 +0,0 @@
-(setq load-path (append '("." "dot_emacs.d"
-                      "dot_emacs.d/programming" "dot_emacs.d/editing"
-                      "dot_emacs.d/writing" "dot_emacs.d/misc"
-                      "dot_emacs.d/gnus") load-path))
